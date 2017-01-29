ae22b983bac0d07b9a1b1a8054fca34b3cba1f87
rndm20 <nathpres@hotmail.com>
2017-01-28 19:13:11 -0700

    Update discord_bot.js

--- discord_bot.js
+++ discord_bot.js
@@ -1,3 +1,88 @@
+var fs = require('fs');
+
+try {
+	var Discord = require("discord.js");
+} catch (e){
+	console.log(e.stack);
+	console.log(process.version);
+	console.log("Please run npm install and ensure it passes with no errors!");
+	process.exit();
+}
+console.log("Starting DiscordBot\nNode version: " + process.version + "\nDiscord.js version: " + Discord.version);
+
+
+
+// Get authentication data
+try {
+	var AuthDetails = require("./auth.json");
+} catch (e){
+	console.log("Please create an auth.json like auth.json.example with a bot token or an email and password.\n"+e.stack);
+	process.exit();
+}
+
+// Load custom permissions
+var dangerousCommands = ["eval","pullanddeploy","setUsername"];
+var Permissions = {};
+try{
+	Permissions = require("./permissions.json");
+} catch(e){
+	Permissions.global = {};
+	Permissions.users = {};
+}
+
+for( var i=0; i<dangerousCommands.length;i++ ){
+	var cmd = dangerousCommands[i];
+	if(!Permissions.global.hasOwnProperty(cmd)){
+		Permissions.global[cmd] = false;
+	}
+}
+Permissions.checkPermission = function (user,permission){
+	try {
+		var allowed = true;
+		try{
+			if(Permissions.global.hasOwnProperty(permission)){
+				allowed = Permissions.global[permission] === true;
+			}
+		} catch(e){}
+		try{
+			if(Permissions.users[user.id].hasOwnProperty(permission)){
+				allowed = Permissions.users[user.id][permission] === true;
+			}
+		} catch(e){}
+		return allowed;
+	} catch(e){}
+	return false;
+}
+fs.writeFile("./permissions.json",JSON.stringify(Permissions,null,2));
+
+//load config data
+var Config = {};
+try{
+	Config = require("./config.json");
+} catch(e){ //no config file, use defaults
+	Config.debug = false;
+	Config.commandPrefix = '!';
+	try{
+		if(fs.lstatSync("./config.json").isFile()){
+			console.log("WARNING: config.json found but we couldn't read it!\n" + e.stack);
+		}
+	} catch(e2){
+		fs.writeFile("./config.json",JSON.stringify(Config,null,2));
+	}
+}
+if(!Config.hasOwnProperty("commandPrefix")){
+	Config.commandPrefix = '!';
+}
+
+var messagebox;
+var aliases;
+try{
+	aliases = require("./alias.json");
+} catch(e) {
+	//No aliases defined
+	aliases = {};
+}
+
 var commands = {	
 	"alias": {
 		usage: "<name> <actual command>",
@@ -98,3 +183,192 @@ var commands = {
 		}
 	}
 };
+
+if(AuthDetails.hasOwnProperty("client_id")){
+	commands["invite"] = {
+		description: "generates an invite link you can use to invite the bot to your server",
+		process: function(bot,msg,suffix){
+			msg.channel.sendMessage("invite link: https://discordapp.com/oauth2/authorize?&client_id=" + AuthDetails.client_id + "&scope=bot&permissions=470019135");
+		}
+	}
+}
+
+
+try{
+	messagebox = require("./messagebox.json");
+} catch(e) {
+	//no stored messages
+	messagebox = {};
+}
+function updateMessagebox(){
+	require("fs").writeFile("./messagebox.json",JSON.stringify(messagebox,null,2), null);
+}
+
+var bot = new Discord.Client();
+
+bot.on("ready", function () {
+	console.log("Logged in! Serving in " + bot.guilds.array().length + " servers");
+	require("./plugins.js").init();
+	console.log("type "+Config.commandPrefix+"help in Discord for a commands list.");
+	bot.user.setGame(Config.commandPrefix+"help | " + bot.guilds.array().length +" Servers"); 
+});
+
+bot.on("disconnected", function () {
+
+	console.log("Disconnected!");
+	process.exit(1); //exit node.js with an error
+
+});
+
+function checkMessageForCommand(msg, isEdit) {
+	//check if message is a command
+	if(msg.author.id != bot.user.id && (msg.content.startsWith(Config.commandPrefix))){
+        console.log("treating " + msg.content + " from " + msg.author + " as command");
+		var cmdTxt = msg.content.split(" ")[0].substring(Config.commandPrefix.length);
+        var suffix = msg.content.substring(cmdTxt.length+Config.commandPrefix.length+1);//add one for the ! and one for the space
+        if(msg.isMentioned(bot.user)){
+			try {
+				cmdTxt = msg.content.split(" ")[1];
+				suffix = msg.content.substring(bot.user.mention().length+cmdTxt.length+Config.commandPrefix.length+1);
+			} catch(e){ //no command
+				msg.channel.sendMessage("Yes?");
+				return;
+			}
+        }
+		alias = aliases[cmdTxt];
+		if(alias){
+			console.log(cmdTxt + " is an alias, constructed command is " + alias.join(" ") + " " + suffix);
+			cmdTxt = alias[0];
+			suffix = alias[1] + " " + suffix;
+		}
+		var cmd = commands[cmdTxt];
+        if(cmdTxt === "help"){
+            //help is special since it iterates over the other commands
+						if(suffix){
+							var cmds = suffix.split(" ").filter(function(cmd){return commands[cmd]});
+							var info = "";
+							for(var i=0;i<cmds.length;i++) {
+								var cmd = cmds[i];
+								info += "**"+Config.commandPrefix + cmd+"**";
+								var usage = commands[cmd].usage;
+								if(usage){
+									info += " " + usage;
+								}
+								var description = commands[cmd].description;
+								if(description instanceof Function){
+									description = description();
+								}
+								if(description){
+									info += "\n\t" + description;
+								}
+								info += "\n"
+							}
+							msg.channel.sendMessage(info);
+						} else {
+							msg.author.sendMessage("**Available Commands:**").then(function(){
+								var batch = "";
+								var sortedCommands = Object.keys(commands).sort();
+								for(var i in sortedCommands) {
+									var cmd = sortedCommands[i];
+									var info = "**"+Config.commandPrefix + cmd+"**";
+									var usage = commands[cmd].usage;
+									if(usage){
+										info += " " + usage;
+									}
+									var description = commands[cmd].description;
+									if(description instanceof Function){
+										description = description();
+									}
+									if(description){
+										info += "\n\t" + description;
+									}
+									var newBatch = batch + "\n" + info;
+									if(newBatch.length > (1024 - 8)){ //limit message length
+										msg.author.sendMessage(batch);
+										batch = info;
+									} else {
+										batch = newBatch
+									}
+								}
+								if(batch.length > 0){
+									msg.author.sendMessage(batch);
+								}
+						});
+					}
+        }
+		else if(cmd) {
+			if(Permissions.checkPermission(msg.author,cmdTxt)){
+				try{
+					cmd.process(bot,msg,suffix,isEdit);
+				} catch(e){
+					var msgTxt = "command " + cmdTxt + " failed :(";
+					if(Config.debug){
+						 msgTxt += "\n" + e.stack;
+					}
+					msg.channel.sendMessage(msgTxt);
+				}
+			} else {
+				msg.channel.sendMessage("You are not allowed to run " + cmdTxt + "!");
+			}
+		} else {
+			msg.channel.sendMessage(cmdTxt + " not recognized as a command!").then((message => message.delete(5000)))
+		}
+	} else {
+		//message isn't a command or is from us
+        //drop our own messages to prevent feedback loops
+        if(msg.author == bot.user){
+            return;
+        }
+
+        if (msg.author != bot.user && msg.isMentioned(bot.user)) {
+                msg.channel.sendMessage(msg.author + ", you called?");
+        } else {
+
+				}
+    }
+}
+
+bot.on("message", (msg) => checkMessageForCommand(msg, false));
+bot.on("messageUpdate", (oldMessage, newMessage) => {
+	checkMessageForCommand(newMessage,true);
+});
+
+//Log user status changes
+bot.on("presence", function(user,status,gameId) {
+	//if(status === "online"){
+	//console.log("presence update");
+	console.log(user+" went "+status);
+	//}
+	try{
+	if(status != 'offline'){
+		if(messagebox.hasOwnProperty(user.id)){
+			console.log("found message for " + user.id);
+			var message = messagebox[user.id];
+			var channel = bot.channels.get("id",message.channel);
+			delete messagebox[user.id];
+			updateMessagebox();
+			bot.sendMessage(channel,message.content);
+		}
+	}
+	}catch(e){}
+});
+
+
+exports.addCommand = function(commandName, commandObject){
+    try {
+        commands[commandName] = commandObject;
+    } catch(err){
+        console.log(err);
+    }
+}
+exports.commandCount = function(){
+    return Object.keys(commands).length;
+}
+if(AuthDetails.bot_token){
+	console.log("logging in with token");
+	bot.login(AuthDetails.bot_token);
+} else {
+	console.log("Logging in with user credentials is no longer supported!\nYou can use token based log in with a user account, see\nhttps://discord.js.org/#/docs/main/master/general/updating");
+}
+Contact GitHub API Training Shop Blog About
+© 2017 GitHub, Inc. Terms Privacy Security Status Help
